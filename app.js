23d71e045ec34f7e253f78e7da3414d89bcd3b13
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-12-03 18:14:10 +0100

    Create POST route for adding products with category check

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./db/index.js";
 import Category from "./models/Category.js";
+import Product from "./models/Product.js";
 import "dotenv/config";
 
 const app = express();
@@ -14,6 +15,12 @@ app.post("/categories", async (req, res) => {
   try {
     const { name } = req.body;
 
+    if (!name || !price || !category) {
+      return res
+        .status(400)
+        .json({ message: "All fields (name, price, category) are required" });
+    }
+
     const existingCategory = await Category.findOne({ name });
 
     if (existingCategory) {
@@ -25,6 +32,35 @@ app.post("/categories", async (req, res) => {
     res.status(201).json(category);
   } catch (error) {
     res.status(500).json({ message: "Failed to create category" });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const { name, price, category } = req.body;
+
+    if (!name || !price || !category) {
+      return res
+        .status(400)
+        .json({ message: "Missing required fields: name, price, or category" });
+    }
+
+    const foundCategory = await Category.findOne({ name: category });
+
+    if (!foundCategory) {
+      return res.status(400).json({ message: "Category not found" });
+    }
+
+    const newProduct = new Product({
+      name,
+      price,
+      category: foundCategory._id,
+    });
+    await newProduct.save();
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create product" });
   }
 });
 
